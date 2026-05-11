845c02f7634e421ee6ec8745730704674d71bff6
eyd149254-crypto <eyd149254@gmail.com>
2026-05-11 23:07:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -33,32 +33,26 @@ app.add_middleware(
 
 
 # =========================================================
-# FIREBASE INIT DEPUIS VARIABLE D'ENVIRONNEMENT
+# FIREBASE INIT — CREDENTIALS HARDCODÉES (DEMO)
 # =========================================================
 
 if not firebase_admin._apps:
 
-    firebase_json = os.environ.get("FIREBASE_CREDENTIALS")
+    cred_dict = {
+        "type": "service_account",
+        "project_id": "safehome-22c06",
+        "private_key_id": "af6ef0587fe1ec928619d80327db191657593c21",
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC65G52R0tTByKV\nzz8RS1LyAFvGZPlnkx3KvjPsz1+/hqWxHxk6nB68lLKoXvs9pugWPV1+1Nyd1Yac\n1+Nm9YLF0Wd+d41TGj5LFIAOrzfcSFTc9YZcCwrRRC+u3iJ9Eka5v3OpQQfjCpq7\neFZLV8GzWBHfeOuEwkravp6ZzvDF/OVcPVXVNpMK693ULMnoPfhGpt1G735jLE5P\nydguy5z5M2ZmwJAWmFhCOnLet2pqy0fx3NGXzuogmzeVrWDASEDGxVbhU8cwlrr9\nz4bJ2uRQ1leDE1/lbRnyQxAFaDhSeoMenVHtIWHjRlyNGjaXiWewkf6yT4kpUdSV\nbPYe10JPAgMBAAECggEAITzyFlAMqeoJcRpILaO3TznYGJsprg2AjWexZWrmLPJd\n8nfE7WMJpmFNutsVlLvj59ynDaD+0YVBqRBOLaf5R4Q8A0Zw2HhA3up48eOgrCkj\nCp8R87m1BU26qz9vY84FzRArGk1qASuIJFI9Cq5DUUmW+CcRv+0sEeY338Ppl1Ra\nh0XLrHW/xysKY8rn+3vfqSHDX4nHAhdReKkdGrGJ9svZpIhhcCmEWLDeyX750q12\nUihwP4sX/2Oeaz8fXOFMlgzqm/bS+kZJp2K+N5f6WGu2IOD/eLjpaOKk+Zn3yd5N\nyMFeTpsCovgDdm3u5sQFX2ehMpcuwK6w7xCt/hvCCQKBgQDe0L3xYQdDuTh/UaCe\n/QPrlJuMN2XFaMEbGc+rYekjyM91SiHlX6jVBOImetHQEH44f3uG4tzSgw9l55Dp\nu7paCXnBa3Ej3iNrTGCU+CQbQcs78vHz14MX5Y9ej4zxe2euE8A4t1c6oe3k2Q36\neG2Qv3A84RjRuT5eHp0KTTaw6QKBgQDWug3OsAFP75pcrfxPJpOJymdgzVuFQZMV\nx5tcsmHRBrUAObt+vfFsXuPV3XYQN/F4kwM+muk0+0NzyZkbhvYWdZJ3/6IcABzt\nzqiY98i7MNg0jlDVm6Cq7zE4R76gk8CS+UoJtxWEXyATq4oA9RrCpiAKqqXV0+oA\n3SaAYx8WdwKBgHZT/MtXb/gzQSG8Q8SrMY3GWeTY9p8jxomR54tob9ulJSdwuxeI\n+axG76LnzszkNWPjr8IucH8LQv4nP/ogzBJMvfBOEsOtkLnG0fPK48Hl6vxnRL7u\nUJw8OStKqNsFYkY8DvUPyK9Gl4PVEWtW0TjxsbVjBT87VTe+oj96SfUhAoGAFC7I\nvXStquXKjIuN+6KUm476yI6E27GeMZ3hbIUXzJ3kxXCnvvH7j0QcBi4ausuHVt36\noBfPc7tqS6fXTE8TMmk6qyzL//XCwld6YWZo569foxD9aBaIs4hoB/I2aMF6iFYG\nL4OV8is+yJqHDi/7o/AZcuc3TzHr/MjMpaSrd2ECgYBV+z7N1ce1eZ0Xf+Wm1wt9\nSDztTzdYYwfCQCHd1m7nobECgO4lQYwY4a2lhCpe4PdCI/C4W/v5FxlGSktjMJR5\nIu+aMtiazniPRrWIvDCG2WYaGpIk/t+LwUm/zHOQVTmumCj+EfxxFBWW0pDMbLcM\nvGOmTyijszGU11K/iwm4rA==\n-----END PRIVATE KEY-----\n",
+        "client_email": "firebase-adminsdk-fbsvc@safehome-22c06.iam.gserviceaccount.com",
+        "client_id": "102725483848353844388",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40safehome-22c06.iam.gserviceaccount.com",
+        "universe_domain": "googleapis.com"
+    }
 
-    if firebase_json:
-        # Production (Railway) : on lit depuis la variable d'env
-        cred_dict = json.loads(firebase_json)
-
-        # Correction des \n dans la clé privée (problème courant Railway)
-        if "private_key" in cred_dict:
-            cred_dict["private_key"] = cred_dict["private_key"].replace("\\n", "\n")
-
-        print("🔑 Private key preview:", cred_dict["private_key"][:60])
-
-        cred = credentials.Certificate(cred_dict)
-    else:
-        # Local : on lit depuis le fichier JSON
-        cred_path = os.path.join(
-            os.path.dirname(__file__),
-            "safehome-22c06-firebase-adminsdk-fbsvc-af6ef0587f.json"
-        )
-        cred = credentials.Certificate(cred_path)
-
+    cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
 
 
